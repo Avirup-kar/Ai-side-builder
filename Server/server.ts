f8341d471ecdee1d51ae888684ea303bbae77029
Avirup kar <avirupk57@gmail.com>
2026-01-07 20:05:54 +0530

    Update server.ts

--- Server/server.ts
+++ Server/server.ts
@@ -9,7 +9,7 @@ import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const corsoptions = {
   origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
